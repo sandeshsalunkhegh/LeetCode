42584845e61cd2cb6a08aad50dbc82159240ad2f
Sandesh Salunkhe <sandesh@Sandesh-Salunkhe.local>
2022-08-14 08:20:56 +0530

    Addes solution for 1929. Concatenation of Array

---
+++ 1929. Concatenation of Array.cpp
@@ -0,0 +1,9 @@
+//1929. Concatenation of Array - https://leetcode.com/problems/concatenation-of-array/
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        for(int i=0;i<n;i++)    nums.push_back(nums[i]);
+        return nums;
+    }
+};
