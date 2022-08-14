7b318708fcb26c7af429f2fdc4abc560425a87da
Sandesh Salunkhe <sandesh@Sandesh-Salunkhe.local>
2022-08-14 08:26:49 +0530

    Added solution for 1920. Build Array from Permutation

---
+++ 1920. Build Array from Permutation.cpp
@@ -0,0 +1,9 @@
+//1920. Build Array from Permutation - https://leetcode.com/problems/build-array-from-permutation/
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)  ans.push_back(nums[nums[i]]);
+        return ans;
+    }
+};
